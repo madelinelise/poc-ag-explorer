3901a31f7e0e429300221f838253b24f2c81d200
madelinelise <madelinelise@gmail.com>
2023-11-10 11:47:19 -0800

    Adds firebase.

---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHUimSTTB6zzOmNw1t4YU13dq4RbWLSZo",
+  authDomain: "poc-ag-explorer.firebaseapp.com",
+  projectId: "poc-ag-explorer",
+  storageBucket: "poc-ag-explorer.appspot.com",
+  messagingSenderId: "770637093726",
+  appId: "1:770637093726:web:380d94adcb92956de0cdcf",
+  measurementId: "G-E6WCSQWHN0"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
